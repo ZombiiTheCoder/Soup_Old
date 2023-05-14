063c31bd4308710cf882e8d04ca016d1f34635ad
Zombi <zombeifedgaming@outlook.com>
2023-05-13 23:41:08 -0500

    remove Extra Imports from package.ts

--- package.ts
+++ package.ts
@@ -1,6 +1,3 @@
-import { ProcessFlags, RunFromConsole } from "./Console.ts";  
-import { Tokenize } from "./lexer/lexer.ts";  
-import Parser from "./parser/parser.ts";  
 import { RunInterpreter } from "./run.ts";  
 import { createGlobalEnviorment } from "./runtime/enviornment.ts";  
 const enviornment = createGlobalEnviorment()  
